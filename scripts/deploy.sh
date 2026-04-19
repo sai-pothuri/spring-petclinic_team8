1e4907170c6f1f693d69780236da58877b830933
YumengLi <yumengl3@andrew.cmu.edu>
2026-04-19 15:30:36 +0800

    fix: explicit directory switch before running ansible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ echo "Bypassing container and invoking host-side Ansible for deployment..."
 
 # Use SSH to connect to the host (10.0.0.50) and run the local ansible-playbook
 # Note: Using the absolute path confirmed by 'ansible --version'
-ssh -o StrictHostKeyChecking=no lili@10.0.0.50 "/home/lili/.local/bin/ansible-playbook /home/lili/spring-petclinic_team8/ansible/deploy.yml"
+# ssh -o StrictHostKeyChecking=no lili@10.0.0.50 "/home/lili/.local/bin/ansible-playbook /home/lili/spring-petclinic_team8/ansible/deploy.yml"
+ssh -o StrictHostKeyChecking=no lili@10.0.0.50 "cd /home/lili/spring-petclinic_team8 && /home/lili/.local/bin/ansible-playbook ansible/deploy.yml"
